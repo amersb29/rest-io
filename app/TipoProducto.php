006c6ae3fe38645cfcbaca0fb75adcbcaed655ec
Amer Salgado Berzunza <amer.salgado@globant.com>
2019-11-11 18:14:53 -0600

    Catálogos 2

--- app/TipoProducto.php
+++ app/TipoProducto.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class TipoProducto extends Model
 {
+    protected $fillable = [
+        'description',
+    ];
+
     public function products()
     {
         $this->hasMany(Product::class);
