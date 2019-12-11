99ca341b6d61ec4d15eb6139b726c25ec994607d
Amer Salgado Berzunza <amer.salgado@globant.com>
2019-12-11 09:50:01 -0600

    Carga de archivos del lado del servidor

---
+++ app/GraphQL/Mutations/Upload.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use Illuminate\Http\UploadedFile;
+
+class Upload
+{
+    const DOCS_FOLDER = "docs";
+    const IMAGE_FOLDER = "image";
+
+    /**
+     * Upload a file, store it on the server and return the path.
+     *
+     * @param  mixed  $root
+     * @param  mixed[]  $args
+     * @return string|null
+     */
+    public function resolve($root, array $args): ?string
+    {
+        /** @var \Illuminate\Http\UploadedFile $file */
+        foreach ($args["files"] as $file) {
+            $file->storePubliclyAs('public/'. $this->getFolderName( $file ) , $file->getClientOriginalName() );
+        }
+
+        return 'Files created';
+    }
+
+    private function getFolderName( $file ): string {
+        $extension = strtolower( $file->getClientOriginalExtension() );
+
+        switch ($extension) {
+            case 'bmp':
+            case 'eps':
+            case 'gif':
+            case 'jpg':
+            case 'jpeg':
+            case 'tif':
+            case 'tiff':
+            case 'png':
+            case 'psd':
+            case 'svg':
+                return self::IMAGE_FOLDER;
+                break;
+            default:
+                return self::DOCS_FOLDER;
+                break;
+        }
+
+        return $folderName;
+    }
+}
